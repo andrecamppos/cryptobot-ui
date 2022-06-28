56f9dde37b6eed86a2c5ef159ee04d5669e50fd2
Andre Campos <andrecamppos@gmail.com>
2022-06-28 17:42:54 +0100

    Title tweaks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cryptobot-ui'`, () => {
+  it(`should have as title 'cryptobot'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cryptobot-ui');
+    expect(app.title).toEqual('cryptobot');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cryptobot-ui app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('cryptobot app is running!');
   });
 });
